c5d0506e282f45d40e0ca540084d1a4a4e3374f0
VikaChereushenko <vkkishko@gmail.com>
2023-09-29 20:02:56 +0300

    js_hw_2

---
+++ js_hw_2/script.js
@@ -0,0 +1,15 @@
+//I don't know what to type
+/*I am typing
+some
+long
+comment
+so 
+that
+practice*/
+console.log("test") console.log(123) //думаю тут помилка,бо немає крапки з комою між командами
+function click(){
+    var element = document.getElementsByClassName('className').innerHTML = 
+    '<h1> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>';
+  }
+  click() 
+  
