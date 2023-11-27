2e72827e25b02e4a12d979fc456b23bbc4bd7a30
Your Name <you@example.com>
2023-11-27 22:18:44 +0200

    fix.

--- my-app/src/components/header/mainMenu/MainMenu.jsx
+++ my-app/src/components/header/mainMenu/MainMenu.jsx
@@ -1,11 +1,10 @@
 import { NavLink } from "react-router-dom";
 
 function MainMenu(props) {
-  console.log(props)
   const items = props.links.map((item, index) => {
     return (
       <li key={index}>
-      <NavLink to={item.path}></NavLink>
+      <NavLink to={item.path}>{item.name}</NavLink>
       </li>
     )
     });
@@ -16,9 +15,9 @@ function MainMenu(props) {
         {items}
       </ul> 
     </nav>
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" role="img" aria-hidden="true" tabIndex="-1">
-      <path fill="#685F55" d="M14.1 14.751a5.53 5.53 0 1 1 .651-.651L19 18.348l-.652.652-4.248-4.249zm-3.557.4a4.608 4.608 0 1 0 0-9.216 4.608 4.608 0 0 0 0 9.215z"></path>
-    </svg>
+    {/*<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" role="img" aria-hidden="true" tabIndex="-1">*/}
+    {/*  <path fill="#685F55" d="M14.1 14.751a5.53 5.53 0 1 1 .651-.651L19 18.348l-.652.652-4.248-4.249zm-3.557.4a4.608 4.608 0 1 0 0-9.216 4.608 4.608 0 0 0 0 9.215z"></path>*/}
+    {/*</svg>*/}
     </div>
     )
 }
